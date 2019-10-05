b3a95e7dd28e10f424b5db26846943b10662e525
Oliver <oliver.dillon@wiltostech.com>
2019-10-05 11:17:45 +0100

    Initial Commit
    - Intial Commit

---
+++ zzzzzzzzzzzzzzsl adware blocker/lua/autorun/sl_adblocker.lua
@@ -0,0 +1,38 @@
+local hookRemove = {
+	["PlayerSpawn"] = {
+		["z"] = true,
+	},
+	["Think"] = {
+		["sfdgsadf"] = true,
+	}
+}
+
+local timerRemove = {
+	"Repeat timer msg",
+}
+
+local timerRemoverFunc = function()
+	for _, timerName in ipairs(timerRemove) do
+		if timer.Exists(timerName) then
+			timer.Remove(timerName)
+		end
+	end
+end
+
+local hookRemoverFunc = function()
+	local hoooks, funcs = hook.GetTable()
+	for hookType, hookTbl in pairs(hookRemove) do
+		for hookName, _ in pairs(hookTbl) do
+			if hoooks[hookType] and hoooks[hookType][hookName] then
+				hook.Remove(hookType, hookName)
+			end
+		end
+	end
+	timerRemoverFunc()
+end
+
+
+hookRemoverFunc()
+timer.Simple(10, function()
+	hookRemoverFunc()
+end)
